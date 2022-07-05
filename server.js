656d6d49a11351bdd88091dcbce1af2e40f268c2
aungkyawnyuntlashio <32254764+aungkyawnyuntlashio@users.noreply.github.com>
2022-07-05 12:27:23 +0630

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, { debug: true });
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/peerjs", peerServer);
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log('join-room')
+    socket.join(roomId);
+    // socket.to(roomId).broadcast.emit("user-connected",userId)
+    io.to(roomId).emit("user-connected", userId);
+    socket.on("message", (message) => {
+      console.log('message call')
+      io.to(roomId).emit("create-message", message);
+    });
+  });
+});
+
+server.listen(3030);
